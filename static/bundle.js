9805a32fe1834a31dd4b7968b59f28e106953be1
Hastings Greer <hastings.greer@kitware.com>
2018-11-05 17:00:49 -0500

    compile for gh-pages

--- static/bundle.js
+++ static/bundle.js
@@ -34671,24 +34671,70 @@ function log(l) {
 var running = false;
 function runmodel() {
   log('getting image');
-  var image = window.webcam.capture();
-  window.image = image;
-  // log("image " + image.shape);
+  var output = _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["tidy"](() => {
+    var image = window.webcam.capture();
+    window.image = image;
+    // log("image " + image.shape);
 
-  log('running model');
-  var output = window.model.predict(image);
-  output = _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["reshape"](output, [128, 128, 2]);
+    log('running model');
+    var output = window.model.predict(image);
 
-  window.output = output;
+    output = _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["reshape"](output, [128, 128, 2]);
 
-  output = _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["concat"]([output, _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["add"](1, _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["mul"](-0.001, output))], 2);
+    window.output = output;
 
-  output = output.add(-.5).mul(3000).clipByValue(0, 1);
+    output = _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["concat"]([output, _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["add"](1, _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["mul"](-0.001, output))], 2);
+
+    return output.add(-0.5).mul(3000).clipByValue(0, 1);
+  });
   _tensorflow_tfjs__WEBPACK_IMPORTED_MODULE_0__["toPixels"](output, document.getElementById('segmentation'));
   log('done');
+
+  setTimeout(skeletonize(), 10);
+  // running = false;
   if (running) {
     setTimeout(runmodel, 100);
   }
+}
+
+function skeletonize() {
+  var mat = cv.imread(document.getElementById('segmentation'));
+  var gr = new cv.Mat();
+  cv.cvtColor(mat, gr, cv.COLOR_RGBA2GRAY, 0);
+  cv.threshold(gr, gr, 150, 256, cv.THRESH_BINARY_INV);
+
+  var skele = cv.Mat.zeros(128, 128, cv.CV_8UC1);
+  var temp = cv.Mat.zeros(128, 128, cv.CV_8UC1);
+  var elem = cv.getStructuringElement(cv.MORPH_CROSS, new cv.Size(3, 3));
+  var i;
+  for (i = 0; i < 12; i++) {
+    cv.morphologyEx(gr, temp, cv.MORPH_OPEN, elem);
+    cv.bitwise_not(temp, temp);
+    cv.bitwise_and(gr, temp, temp);
+    cv.bitwise_or(skele, temp, skele);
+    cv.erode(gr, gr, elem);
+  }
+
+  cv.imshow('skeleton', skele);
+  mat.delete();gr.delete();temp.delete();
+  let dst = cv.Mat.zeros(skele.rows, skele.cols, cv.CV_8UC3);
+  let lines = new cv.Mat();
+
+  cv.HoughLines(skele, lines, 1, Math.PI / 180, 30, 0, 0, 0, Math.PI);
+  // draw lines
+  for (let i = 0; i < lines.rows; ++i) {
+    let rho = lines.data32F[i * 2];
+    let theta = lines.data32F[i * 2 + 1];
+    let a = Math.cos(theta);
+    let b = Math.sin(theta);
+    let x0 = a * rho;
+    let y0 = b * rho;
+    let startPoint = { x: x0 - 1000 * b, y: y0 + 1000 * a };
+    let endPoint = { x: x0 + 1000 * b, y: y0 - 1000 * a };
+    cv.line(dst, startPoint, endPoint, [255, 0, 0, 255]);
+  }
+  cv.imshow('lines', dst);
+  dst.delete();lines.delete();
 }
 
 async function run() {
